59d1edf96fc306deca7462224b27542030baeb96
shmily <go4thx@gmail.com>
2017-06-08 12:51:42 +0900

    posion layout

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './style.css';
 
+const side = 32
+
 class Map extends Component {
   render() {
-    const { type } = this.props
+    const { type, index } = this.props
+    const [left, top] = [(index % 9)*side, parseInt(index / 9, 10)*side]
     return (
-      <div className={`map map-${type}`}>
+      <div
+       style={{left, top}}
+       className={`map map-${type}`}>
       </div>
     );
   }
