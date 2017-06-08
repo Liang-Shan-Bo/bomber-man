867b3aab144e5db658f7591c9a0dfd44872285f2
shmily <go4thx@gmail.com>
2017-06-08 14:40:31 +0900

    update

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -6,12 +6,13 @@ const side = 32
 class Map extends Component {
   render() {
     const { type, index } = this.props
+    //二维数组的话 下面就不用算了，直接就是横纵坐标
     const [left, top] = [(index % 9)*side, parseInt(index / 9, 10)*side]
     return (
       <div
        style={{left, top}}
-       className={`map map-${type}`}>
-      </div>
+       className={`map map-${type}`} 
+      />
     );
   }
 }
