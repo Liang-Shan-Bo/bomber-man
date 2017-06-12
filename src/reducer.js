7270415c4b26d6333c6d4553a7f87ee80552b964
shmily <go4thx@gmail.com>
2017-06-12 09:59:29 +0900

    single reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,42 +1,32 @@
-import { combineReducers } from 'redux'
 import {
   MAN_MOVE,
 } from './action'
 
-const map = (state = {
-  maps:[['lawn', 'wall', 'lawn', 'lawn', 'lawn', 'power', 'wall', 'lawn', 'lawn'],
+export default (state = {
+  map:{
+    maps:[['lawn', 'wall', 'lawn', 'lawn', 'lawn', 'power', 'wall', 'lawn', 'lawn'],
           ['lawn', 'iron', 'lawn', 'iron', 'lawn', 'iron', 'lawn', 'iron', 'lawn'],
           ['wall', 'lawn', 'lawn', 'wall', 'lawn', 'lawn', 'wall', 'lawn', 'lawn'],
           ['lawn', 'iron', 'lawn', 'iron', 'lawn', 'iron', 'lawn', 'iron', 'lawn'],
           ['lawn', 'lawn', 'lawn', 'lawn', 'wall', 'lawn', 'wall', 'lawn', 'door']]
-}, action) => {
-  switch (action.type) {
-    default:
-      return state
+  },
+  man: {
+    x:0,
+    y:0,
+    type: 'left',
   }
-}
-
-const man = (state = {
-  x:0,
-  y:0,
-  type: 'left',
 }, action) => {
   switch (action.type) {
     case MAN_MOVE:
+      const { x, y} = action.man
       return {
         ...state,
-        x: action.man.x,
-        y: action.man.y,
+        man: {
+          x,
+          y,
+        }
       }
     default:
       return state
   }
 }
-
-
-const rootReducer = combineReducers({
-  map,
-  man,
-})
-
-export default rootReducer
